f1943bf110b815a13adfb5c4adec58ba7523715b
deppy0 <dependant1b@gmail.com>
2026-05-03 10:57:48 +0800

    updated supabase.js

--- server/supabase.js
+++ server/supabase.js
@@ -3,3 +3,5 @@ dotenv.config({ quiet: true });
 
 const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+module.exports = supabase;
