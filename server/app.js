2f1c3b5a0f7b3568c59a05f00c916d9db180f71a
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2026-05-07 11:44:25 +0800

    feat(health): added health route for cron job - deployment related

--- server/app.js
+++ server/app.js
@@ -13,6 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.get('/ping', async (req,res) => {
+  return res.status(200).send('OK');
+});
 app.use('/server/example', require('./routes/example'));
 app.use('/server/user', require('./routes/userRoutes'));
 app.use('/server/auth', require('./routes/auth'));
