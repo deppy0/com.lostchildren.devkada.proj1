acb5d698f55136a2cafc3fe2a188acc4a1221787
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2026-05-07 09:17:52 +0800

    fix(today_sched): added date param - to allow getting of "todays" sched with the designated date

--- server/routes/schedule.js
+++ server/routes/schedule.js
@@ -5,7 +5,8 @@ const router = require('express').Router();
 router.post('/today', auth, async function(req, res) {
 	try {
 		const user_id = req.user.id;
-		const schedule = await schedule_service.getTodaySchedule(user_id);
+		const date = req.params.date;
+		const schedule = await schedule_service.getTodaySchedule(user_id, date);
 		return res.json({ success: true, schedule });
 	} catch (error) {
 		return res.status(error.status || 500).json({ error: error.message || 'Unexpected error' });
